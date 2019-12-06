ee61a912c5d0682754a6192a50f1c6d4410a045f
huye <1031377677@qq.com>
2019-12-06 23:11:45 +0800

    重构任务执行

--- worker/ExecuteMgr.go
+++ worker/ExecuteMgr.go
@@ -1,8 +1,8 @@
 package worker
 
 import (
-	"context"
 	"crontab/common"
+	"crontab/model"
 	"math/rand"
 	"os/exec"
 	"time"
@@ -20,32 +20,13 @@ func InitExecute() {
 }
 
 //执行任务
-func (executor *Executor) ExecutingJob(jobExecuteInfo *common.JobExecuteInfo) {
+func (executor *Executor) ExecutingJob(jobExecuteInfo *common.JobExecuteInfo, pipelineJob *model.PipelineJob) (jobRecord *model.JobRecord) {
 
 	var (
-		cmd        *exec.Cmd
-		output     []byte
-		err        error
-		startTime  time.Time
-		endTime    time.Time
-		jobResult  *common.JobExecuteResult
-		cancelCtx  context.Context
-		cancelFunc context.CancelFunc
+		err error
 	)
-
-	cancelCtx, cancelFunc = context.WithCancel(context.TODO())
-
-	jobExecuteInfo.CancelFunc = cancelFunc
-	jobExecuteInfo.CancelCtx = cancelCtx
-
-	//这是为了以防任务执行出错，没有输出
-	jobResult = &common.JobExecuteResult{
-		JobExecuteInfo: jobExecuteInfo,
-		Output:         make([]byte, 0),
-	}
-
 	//创建锁
-	GJobMgr.CreateLock(jobExecuteInfo.Job.Name)
+	GJobMgr.CreateLock(pipelineJob.JobId)
 
 	//上锁
 	//随机随眠(0~1s),防止锁被一个进程占用
@@ -58,24 +39,60 @@ func (executor *Executor) ExecutingJob(jobExecuteInfo *common.JobExecuteInfo) {
 	}
 	defer GJobLock.Unlock() //任务执行完成后释放锁
 
-	//获得job实际运行时间
+	if pipelineJob.Retries == 0 {
+		//执行任务
+		jobRecord = executor.ExecJob(jobExecuteInfo, pipelineJob.Job)
+	} else {
+		for i := 0; i < pipelineJob.Retries; i++ {
+			//执行任务
+			jobRecord = executor.ExecJob(jobExecuteInfo, pipelineJob.Job)
+			if jobRecord.Status == 1 {
+				break
+			}
+		}
+	}
+	return
+}
+
+func (executor *Executor) ExecJob(jobExecuteInfo *common.JobExecuteInfo, Job *model.Job) (jobRecord *model.JobRecord) {
+	var (
+		cmd       *exec.Cmd
+		output    []byte
+		err       error
+		startTime time.Time
+		endTime   time.Time
+		resChan   chan struct {
+			output string
+			err    error
+		}
+		res struct {
+			output string
+			err    error
+		}
+	)
+	jobRecord = &model.JobRecord{}
 	startTime = time.Now()
-	//执行任务
-	cmd = exec.CommandContext(cancelCtx, "C:\\Windows\\System32\\bash.exe", "-c", jobExecuteInfo.Job.Command)
-	//cmd = exec.Command("C:\\Windows\\System32\\bash.exe", "-c", jobExecuteInfo.Job.Command)
-
-	//获得任务结束时间
+	go func() {
+		cmd = exec.CommandContext(jobExecuteInfo.CancelCtx, "C:\\Windows\\System32\\bash.exe", "-c", Job.Command)
+		output, err = cmd.CombinedOutput()
+		resChan <- struct {
+			output string
+			err    error
+		}{output: string(output), err: err}
+	}()
+	GLogMgr.WriteLog("任务执行完成，输出：" + res.output)
 	endTime = time.Now()
-	//获取输出
-	output, err = cmd.CombinedOutput()
-
-	jobResult.EndTime = endTime
-	jobResult.StartTime = startTime
-	jobResult.Err = err
-	jobResult.Output = output
-
-	GLogMgr.WriteLog("任务执行完成，输出：" + string(output))
-
-	//任务执行完成后，需要通知调度器任务执行完毕
-	GScheduler.PushSchedulerResult(jobResult)
+	res = <-resChan
+	jobRecord.Content = Job.Command
+	jobRecord.Result = res.output
+	jobRecord.JobId = Job.JobId
+	jobRecord.BeginWith = startTime
+	jobRecord.FinishWith = endTime
+	jobRecord.Duration = int(endTime.Sub(startTime).Seconds())
+	if res.err != nil {
+		jobRecord.Status = 0
+	} else {
+		jobRecord.Status = 1
+	}
+	return
 }
