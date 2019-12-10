f42be2e975468ec280839d846028588bcffcff20
huye <1031377677@qq.com>
2019-12-10 11:01:29 +0800

    重构

--- common/Protocol.go
+++ common/Protocol.go
@@ -47,21 +47,8 @@ type JobExecuteResult struct {
 	PiplineRecord *model.PipelineRecord
 }
 
-//日志信息
-type JobLog struct {
-	JobName      string `json:"jobName" bson:"jobName"`           //任务名字
-	Command      string `json:"command" bson:"command"`           //脚本命令
-	Err          string `json:"err" bson:"err"`                   //错误原因
-	Output       string `json:"output" bson:"output"`             //脚本输出
-	PlanTime     int64  `json:"planTime" bson:"planTime"`         //计划开始时间
-	SchedultTime int64  `json:"scheduleTime" bson:"scheduleTime"` //实际调度时间
-	StartTime    int64  `json:"startTime" bson:"startTime"`       // 任务执行开始时间
-	EndTime      int64  `json:"endTime" bson:"endTime"`           //任务执行结束时间
-	Runtime      int64  `json:"runtime" bson:"runtime"`           //任务运行时间
-}
-
 type LogBatch struct {
-	Logs []interface{}
+	Logs []*JobExecuteResult
 }
 
 func BuildJobSchedulePlan(pipeline *model.Pipeline) (jobSchedulePlan *JobSchedulePlan, err error) {
